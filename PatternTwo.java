9ab48ca2a6236d3b22747d3c8905efcd46cefe69
Haritha Reddy <kambhamharitha49@gmail.com>
2025-01-31 05:16:46 +0530

    Add files via upload

---
+++ PatternTwo.java
@@ -0,0 +1,27 @@
+public class PatternTwo {
+    public static void main(String[] args) {
+        int n = 3; // Controls the height (3 rows for top, 2 for bottom)
+        
+        // Upper part of the diamond
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        // Lower part of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
